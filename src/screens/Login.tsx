f9710385d1302c0400f901a1a30ae9f7e79cceeb
bourkison <harrisonbourke97@gmail.com>
2022-01-19 03:23:58 +1100

    Added business name and styling on login component

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -7,6 +7,7 @@ import {
     Text,
     ActivityIndicator,
     SafeAreaView,
+    Pressable,
 } from 'react-native';
 
 import {useAppSelector, useAppDispatch} from '@/store/hooks';
@@ -19,13 +20,6 @@ const Login = () => {
     const [password, setPassword] = useState('');
     const [isLoggingIn, setIsLoggingIn] = useState(false);
     const fetchingStatus = useAppSelector(state => state.user.status);
-    const loggedInUsername = useAppSelector(state => {
-        if (state.user.docData && state.user.docData.username) {
-            return state.user.docData.username;
-        }
-
-        return '';
-    });
 
     const dispatch = useAppDispatch();
 
@@ -41,7 +35,11 @@ const Login = () => {
         }
     };
 
-    let buttonComp = <Button title="Login" onPress={login} />;
+    let buttonComp = (
+        <Pressable style={styles.loginButton} onPress={login}>
+            <Text style={styles.loginButtonText}>Login</Text>
+        </Pressable>
+    );
 
     if (fetchingStatus === 'loading' || isLoggingIn) {
         buttonComp = <ActivityIndicator />;
@@ -49,11 +47,31 @@ const Login = () => {
 
     return (
         <SafeAreaView style={styles.container}>
-            <View style={styles.container}>
-                <Text>{loggedInUsername}</Text>
-                <TextInput placeholder="Username" onChangeText={setUsername} />
-                <TextInput placeholder="Password" onChangeText={setPassword} />
-                {buttonComp}
+            <View style={styles.formContainer}>
+                <Text style={styles.headers}>STRENIVE</Text>
+                <TextInput
+                    placeholder="Username"
+                    onChangeText={setUsername}
+                    autoComplete="off"
+                    autoCapitalize="none"
+                    onSubmitEditing={login}
+                    style={styles.textInput}
+                />
+                <TextInput
+                    placeholder="Password"
+                    onChangeText={setPassword}
+                    autoCapitalize="none"
+                    autoComplete="off"
+                    secureTextEntry={true}
+                    onSubmitEditing={login}
+                    style={styles.textInput}
+                />
+                <View style={styles.buttonsCont}>
+                    <Pressable style={styles.signUpButton} onPress={login}>
+                        <Text style={styles.signUpButtonText}>Sign Up</Text>
+                    </Pressable>
+                    {buttonComp}
+                </View>
             </View>
         </SafeAreaView>
     );
@@ -64,6 +82,59 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        width: '100%',
+    },
+    formContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        padding: 20,
+    },
+    headers: {
+        fontSize: 48,
+        marginBottom: 10,
+    },
+    buttonsCont: {
+        flexDirection: 'row',
+        height: 48,
+        marginTop: 10,
+    },
+    signUpButton: {
+        padding: 10,
+        borderColor: '#f194ff',
+        borderWidth: 4,
+        marginRight: 3,
+        flex: 1,
+        justifyContent: 'center',
+    },
+    signUpButtonText: {
+        color: '#f194ff',
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    loginButton: {
+        backgroundColor: '#f194ff',
+        padding: 10,
+        marginLeft: 3,
+        flex: 1,
+        justifyContent: 'center',
+    },
+    loginButtonText: {
+        color: '#FFF',
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    textInput: {
+        width: '100%',
+        borderColor: 'black',
+        borderWidth: 1,
+        textAlign: 'center',
+        fontSize: 16,
+        padding: 5,
+        margin: 5,
     },
 });
 
